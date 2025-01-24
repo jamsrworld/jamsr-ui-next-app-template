13f3906ed29780605668f18c13dcbb715d74b5eb
jaickykumar9771 <jamsrworld@gmail.com>
2025-01-24 13:11:54 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { UIProvider } from "@jamsr-ui/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+         <UIProvider>
+          {children}
+        </UIProvider>
       </body>
     </html>
   );
