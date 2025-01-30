dbd73fa9bda4689895d765d4b4fdb5f1f4c64e9f
adityaa72 <adityah1357908642@gmail.com>
2025-01-30 07:53:10 +0530

    feat: tailwind4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import { Button } from "@jamsr-ui/react";
+import { Button, Text } from "@jamsr-ui/react";
+
 export default function Home() {
   return (
-    <div className="flex gap-4 justify-center items-center h-dvh">
+    <div className="flex flex-col gap-4 justify-center items-center h-dvh">
+      <h1 className="text-5xl bg-content1">Hello world!</h1>
+      <Text as="h2" variant="h3">
+        Hello World! 2
+      </Text>
       <Button color="default">Default</Button>
       <Button color="primary">Primary</Button>
       <Button color="secondary">Secondary</Button>
